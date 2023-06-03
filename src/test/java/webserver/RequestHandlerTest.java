efe9958a41144689ea39cf43c92bff744ffb2455
Byeongsu KIM <brightdev.bs@gmail.com>
2023-06-03 15:37:15 +0200

    Next Step - 실습 3-1

---
+++ src/test/java/webserver/RequestHandlerTest.java
@@ -0,0 +1,32 @@
+package webserver;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestHandlerTest {
+
+    @DisplayName("BufferedReader 테스트")
+    @Test
+    public void BufferdReaderTest() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("filetest.txt"));
+        String s = br.readLine();
+        assertEquals("file test", s);
+
+        s = br.readLine();
+        assertEquals("I want to check if BufferedReader collect all the data in this file", s);
+    }
+
+    @DisplayName("split 테스트")
+    @Test
+    public void splitTest() {
+        String text = "GET /index.html HTTP/1.1";
+        String[] tokens = text.split(" ");
+        assertEquals(tokens[1], "/index.html");
+    }
+
+
+}
