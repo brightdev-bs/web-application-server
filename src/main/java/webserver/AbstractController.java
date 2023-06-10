45ab364b29a5925417b08905b3054a766ccc0dd2
Byeongsu KIM <brightdev.bs@gmail.com>
2023-06-10 14:44:28 +0200

    Next Step - 실습 5-1 다형성을 이용한 리팩토링

---
+++ src/main/java/webserver/AbstractController.java
@@ -0,0 +1,26 @@
+package webserver;
+
+import java.io.IOException;
+
+// 만약 같은 URL로 다양한 Http Method가 사용된다면 이를 상속하여 사용하자.
+public class AbstractController implements Controller {
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws IOException {
+        HttpMethod method = request.getMethod();
+
+        if (method.isPost()) {
+            doPost(request, response);
+        } else {
+            doGet(request, response);
+        }
+    }
+
+    protected void doPost(HttpRequest request, HttpResponse response) {
+
+    }
+
+    protected void doGet(HttpRequest request, HttpResponse response) {
+
+    }
+}
